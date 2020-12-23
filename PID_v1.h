e1d7c88ce9eade537858746abdefa29edc861ab6
caicx <caichaoxi@gmail.com>
2020-12-23 11:57:14 +0800

    prepare to adapt to qt wrapper

--- PID_v1.h
+++ PID_v1.h
@@ -24,6 +24,7 @@ class PID
     PID(double*, double*, double*,        // * constructor.  links the PID to the Input, Output, and 
         double, double, double, int);     //   Setpoint.  Initial tuning parameters are also set here
 	
+    PID(double* input, double* output, double* setpoint); // by phantom
     void SetMode(int Mode);               // * sets PID to either Manual (0) or Auto (non-0)
 
     bool Compute();                       // * performs the PID calculation.  it should be
